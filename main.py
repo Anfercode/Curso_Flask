240be30ce18eb0ade4176ef134e143be6a817a0b
“Andres <andesecci12@gmail.com>
2019-07-07 12:09:33 -0500

    Se hace el request a la ip del usuario

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-    return 'Hello World Platzi'
+    user_ip = request.remote_addr
+    return f'Hello World Platzi, tu IP es {user_ip}'
