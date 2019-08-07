335dae517a4230bc914282083814f06b20d7fa59
“Andres <andesecci12@gmail.com>
2019-08-06 22:01:03 -0500

    Se adiciona el manejo de creacion de tareas

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from flask import request, make_response, redirect, render_template, session, ur
 import unittest
 from flask_login import login_required, current_user
 from app import create_app
-from app.firestore_service import get_users, get_todos
-from app.forms import LoginForm
+from app.firestore_service import get_users, get_todos, put_todo
+from app.forms import TodoForm
 
 app = create_app()
 
 
-todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
+todos = ['Comprar cafe', 'Enviar solicitud de compra',
+         'Entregar video a productor ']
 
 
 @app.cli.command()
@@ -37,18 +38,25 @@ def index():
     return response
 
 
-@app.route('/hello', methods=['GET'])
+@app.route('/hello', methods=['GET', 'POST'])
 @login_required
 def hello():
     user_ip = session.get('user_ip')
     username = current_user.id
+    todo_form = TodoForm()
 
     context = {
         'user_ip': user_ip,
         'todos': get_todos(user_id=username),
-        'username': username
+        'username': username,
+        'todo_form': todo_form
     }
 
-    users = get_users()
+    if todo_form.validate_on_submit():
+        put_todo(username, todo_form.description.data)
+
+        flash('Tu tarea se creo con exito')
+
+        return redirect(url_for('hello'))
 
     return render_template('hello.html', **context)
