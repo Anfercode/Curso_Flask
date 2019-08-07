a88c9e00d21b6a0a2dfbf088d91e56d4582f54ef
“Andres <andesecci12@gmail.com>
2019-08-06 21:25:06 -0500

    Se adiciona la insercion de nuevos usuarios

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,10 @@
 from flask import render_template, session, redirect, flash, url_for
 from flask_login import login_user, login_required, logout_user
 from app.forms import LoginForm
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from . import auth
-from app.firestore_service import get_user
+from app.firestore_service import get_user, user_put
 from app.models import UserData, UserModel
 
 
@@ -21,9 +22,9 @@ def login():
         user_doc = get_user(username)
 
         if user_doc.to_dict() is not None:
-            password_from_db = user_doc.to_dict()['password']
 
-            if password == password_from_db:
+            password_from_db = user_doc.to_dict()['password']
+            if check_password_hash(password_from_db, password):
                 user_data = UserData(username, password)
                 user = UserModel(user_data)
 
@@ -42,6 +43,39 @@ def login():
         return redirect(url_for('index'))
 
     return render_template('login.html', **context)
+
+
+@auth.route('signup', methods=['GET', 'POST'])
+def signup():
+    signup_form = LoginForm()
+    context = {
+        'signup_form': signup_form
+    }
+
+    if signup_form.validate_on_submit():
+        username = signup_form.username.data
+        password = signup_form.password.data
+
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is None:
+            password_hash = generate_password_hash(password)
+            user_data = UserData(username, password_hash)
+            user_put(user_data)
+
+            user = UserModel(user_data)
+
+            login_user(user)
+
+            flash('Bienvenido')
+
+            return redirect(url_for('hello'))
+
+        else:
+            flash('el usuario ya existe')
+
+    return render_template('signup.html', **context)
+
 
 @auth.route('logout')
 @login_required
